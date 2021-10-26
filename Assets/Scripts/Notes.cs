4806a7dc49f84f02df2fd020cf1a4f2439eab27b
Ruffi97 <ruffinigeoffrey@outlook.fr>
2021-10-26 15:08:36 +0200

    pathfinding

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class Notes : MonoBehaviour
 {
-    private GameObject _dest;
-    private Vector3 _destPos;
+    GameObject[] _possibleDest;
+
+    private Transform _dest;
+    private GameObject _nearestDest = null;
     [SerializeField]
     private float _speed;
+    private float _shortestDest;
+    private float _distanceToDest;
 
     void Start()
     {
-        _dest = GameObject.FindGameObjectWithTag("dest");
-        _destPos = _dest.transform.position;
+        _possibleDest = GameObject.FindGameObjectsWithTag("dest");
+        _shortestDest = Mathf.Infinity;
+        
     }
 
     void Update()
     {
-        transform.position += new Vector3(_destPos.x - transform.position.x, _destPos.y - transform.position.y, 0) * _speed * Time.deltaTime;
+        foreach (GameObject dest in _possibleDest)
+        {
+            _distanceToDest = Vector3.Distance(transform.position, dest.transform.position);
+            if(_distanceToDest < _shortestDest)
+            {
+                _shortestDest = _distanceToDest;
+                _nearestDest = dest;
+            }
+        }
+        if (_nearestDest != null)
+        {
+            _dest = _nearestDest.transform;
+
+            transform.position += new Vector3((_dest.transform.position.x - 1f) - transform.position.x, (_dest.transform.position.y - 1f) - transform.position.y,
+            _dest.transform.position.z - transform.position.z) * _speed * Time.deltaTime;
+        }
+
+        
     }
 
     public void OnTriggerEnter(Collider other)
