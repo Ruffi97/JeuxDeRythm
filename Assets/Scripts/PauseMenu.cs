7e65d65eff2b202d4a89c96c2f6be0fed75cb9f2
Fuzu-git <valentin.macaluso@gmail.com>
2021-11-05 22:17:23 +0100

    PauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject PauseFirstButton;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+            else
+            {
+                Pause();
+                EventSystem.current.SetSelectedGameObject(PauseFirstButton);
+
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        
+        
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
