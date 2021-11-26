bbd5e11ce94156443798838fc7f4bec586530a50
Lunyteal <martin.alexis.83600@gmail.com>
2021-11-26 15:59:05 +0100

    animation ville en cours

---
+++ Assets/SpriteSequence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public partial class LevelScrolling
+{
+    [Serializable]
+    public struct SpriteSequence
+    {
+        public Sprite Sprite;
+        public int Number;
+    }
+
+}
