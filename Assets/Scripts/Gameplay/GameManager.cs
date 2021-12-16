5788e5f4115794b275a1cebbb36fcf7eb992d761
Ruffini <ruffinigeoffrey@gmail.com>
2021-12-16 23:00:59 +0100

    scoring

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class GameManager : MonoBehaviour
@@ -18,7 +19,9 @@ public class GameManager : MonoBehaviour
     private float BPM;
     [SerializeField]
     private float _endSheetMusic;
-    
+    [SerializeField]
+    private Text _scoreText;
+
     private Transform SpawnerSelected;
     private float _timer;
     private int index;
@@ -27,15 +30,16 @@ public class GameManager : MonoBehaviour
     private static float _score = 0f;
     private static int _comboLenght = 0;
     private static float _comboMultiplier = 0f;
-    private Random rnd = new Random();
+    private Random rnd = new Random();  
 
     private void Start()
-    {  
+    {     
         access = this;
         _beatsDelay = 60f / BPM;
     }
     private void Update()
     {
+        _scoreText.text = "" + _score;
         index = rnd.Next(0, 7);
         SpawnerSelected = spawnPoints[index];
 
@@ -66,6 +70,10 @@ public class GameManager : MonoBehaviour
             _comboMultiplier = 0.0f;
         }
         else { }
+        if (_score <= 0)
+        {
+            _score = 0f;
+        }
         Debug.Log("Combo : " + _comboLenght);
         Debug.Log("Score : " + _score);
         return;
