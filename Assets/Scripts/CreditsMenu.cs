919ec3a568d4d0cfde8f392fb689b42099e60ea8
Fuzu <valentin.macaluso@gmail.com>
2021-12-16 18:00:21 +0100

    Scenes imp

---
+++ Assets/Scripts/CreditsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsMenu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Credits");
+    }
+}
